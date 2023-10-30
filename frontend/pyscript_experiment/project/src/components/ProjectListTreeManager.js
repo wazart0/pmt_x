6d971300daf151774a3600fcb1a7b2edc7d43ced
awaz <arturwaz11@gmail.com>
2023-10-30 15:20:32 +0100

    commenting unnecessary code

--- frontend/pyscript_experiment/project/src/components/ProjectListTreeManager.js
+++ frontend/pyscript_experiment/project/src/components/ProjectListTreeManager.js
@@ -34,152 +34,152 @@ export function showSubTree(data, id) { // assumption is that the data is sorted
 
 
 
-function changeParent_v1(data, id, parent_id) { // returns true if data has been changed, false if no modifications were made
+// function changeParent_v1(data, id, parent_id) { // returns true if data has been changed, false if no modifications were made
 
-    /* 
-        Cannot make it working something is totally wrong, do not use it for now.
-    */
-
-
-    // check if valid (basic, check if parent_id is a child of id)
-    if (data[id].parent === parent_id) return false
-    if (id === parent_id || parent_id < 0 || parent_id >= data.length) return false
-    if (data[id].wbs !== null && !isString(data[id].wbs)) return false
-
-    if (parent_id !== null) {
-        if (!Number.isInteger(parent_id) || data[parent_id].wbs === null) return false
-
-        let check_subtree = [id]
-        for (let index in data) {
-            if (check_subtree.includes(data[index].parent)) check_subtree.push(data[index].id)
-        }
-        if (check_subtree.includes(parent_id)) return false;
-    } 
+//     /* 
+//         Cannot make it working something is totally wrong, do not use it for now.
+//     */
 
 
-    // determine number of child for new parent
-    let no_of_new_siblings = 1
-    for (let index in data) {
-        if (data[index].parent === parent_id && data[index].wbs !== null) {
-            no_of_new_siblings = no_of_new_siblings + 1
-        }
-    }
+//     // check if valid (basic, check if parent_id is a child of id)
+//     if (data[id].parent === parent_id) return false
+//     if (id === parent_id || parent_id < 0 || parent_id >= data.length) return false
+//     if (data[id].wbs !== null && !isString(data[id].wbs)) return false
+
+//     if (parent_id !== null) {
+//         if (!Number.isInteger(parent_id) || data[parent_id].wbs === null) return false
+
+//         let check_subtree = [id]
+//         for (let index in data) {
+//             if (check_subtree.includes(data[index].parent)) check_subtree.push(data[index].id)
+//         }
+//         if (check_subtree.includes(parent_id)) return false;
+//     } 
 
 
-    // rebuild wbs of old parent and without child (eg. if project was first child) - needed to keep the order
-    let parent_id_old = data[id].parent
+//     // determine number of child for new parent
+//     let no_of_new_siblings = 1
+//     for (let index in data) {
+//         if (data[index].parent === parent_id && data[index].wbs !== null) {
+//             no_of_new_siblings = no_of_new_siblings + 1
+//         }
+//     }
+
+
+//     // rebuild wbs of old parent and without child (eg. if project was first child) - needed to keep the order
+//     let parent_id_old = data[id].parent
     
-    if (parent_id_old !== null) {
-        let subtree_parent_old_IDs = [parent_id_old]
-        for (let index in data) {
-            if (data[index].id !== id && subtree_parent_old_IDs.includes(data[index].parent)) subtree_parent_old_IDs.push(data[index].id)
-        }
+//     if (parent_id_old !== null) {
+//         let subtree_parent_old_IDs = [parent_id_old]
+//         for (let index in data) {
+//             if (data[index].id !== id && subtree_parent_old_IDs.includes(data[index].parent)) subtree_parent_old_IDs.push(data[index].id)
+//         }
 
-        data[parent_id_old]["hasChildren"] = subtree_parent_old_IDs.length > 1 ? true : false
+//         data[parent_id_old]["hasChildren"] = subtree_parent_old_IDs.length > 1 ? true : false
 
-        data[id].parent = parent_id
+//         data[id].parent = parent_id
         
-        if (data[parent_id_old]["hasChildren"]) {
+//         if (data[parent_id_old]["hasChildren"]) {
 
-            let wbs_parent_old = data[parent_id_old].wbs
-            let new_number = 0
-            let wbs_length
-            for (let index in subtree_parent_old_IDs) {
-                if (data[subtree_parent_old_IDs[index]].parent === parent_id_old) {
-                    wbs_length = data[subtree_parent_old_IDs[index]].wbs.length
-                    new_number = new_number + 1
-                }
-                if (subtree_parent_old_IDs[index] !== parent_id_old) data[subtree_parent_old_IDs[index]].wbs = wbs_parent_old + '.' + new_number + data[subtree_parent_old_IDs[index]].wbs.slice(wbs_length)
-            }
-        }
-    } else {
-        data[id].parent = parent_id
-        let new_number = 0
-        let wbs_length
-        for (let index in data) {
-            if (data[index].wbs !== null) {
-                if (data[index].parent === parent_id_old) {
-                    wbs_length = data[index].wbs.length
-                    new_number = new_number + 1
-                }
-                data[index].wbs = new_number + data[index].wbs.slice(wbs_length)
-            }
-        }
-    }
+//             let wbs_parent_old = data[parent_id_old].wbs
+//             let new_number = 0
+//             let wbs_length
+//             for (let index in subtree_parent_old_IDs) {
+//                 if (data[subtree_parent_old_IDs[index]].parent === parent_id_old) {
+//                     wbs_length = data[subtree_parent_old_IDs[index]].wbs.length
+//                     new_number = new_number + 1
+//                 }
+//                 if (subtree_parent_old_IDs[index] !== parent_id_old) data[subtree_parent_old_IDs[index]].wbs = wbs_parent_old + '.' + new_number + data[subtree_parent_old_IDs[index]].wbs.slice(wbs_length)
+//             }
+//         }
+//     } else {
+//         data[id].parent = parent_id
+//         let new_number = 0
+//         let wbs_length
+//         for (let index in data) {
+//             if (data[index].wbs !== null) {
+//                 if (data[index].parent === parent_id_old) {
+//                     wbs_length = data[index].wbs.length
+//                     new_number = new_number + 1
+//                 }
+//                 data[index].wbs = new_number + data[index].wbs.slice(wbs_length)
+//             }
+//         }
+//     }
 
 
-    // rebuild WBS of project and its children
-    let wbs_new = (parent_id === null ? String(no_of_new_siblings) : data[parent_id].wbs + '.' + no_of_new_siblings)
+//     // rebuild WBS of project and its children
+//     let wbs_new = (parent_id === null ? String(no_of_new_siblings) : data[parent_id].wbs + '.' + no_of_new_siblings)
 
-    if (data[id].hasChildren) {
-        let wbs_current_length = data[id].wbs.length
+//     if (data[id].hasChildren) {
+//         let wbs_current_length = data[id].wbs.length
 
-        let subtree_old_IDs = [id]
-        for (let index in data) {
-            if (subtree_old_IDs.includes(data[index].parent)) subtree_old_IDs.push(data[index].id)
-        }
+//         let subtree_old_IDs = [id]
+//         for (let index in data) {
+//             if (subtree_old_IDs.includes(data[index].parent)) subtree_old_IDs.push(data[index].id)
+//         }
 
-        for (let index in subtree_old_IDs) {
-            if (subtree_old_IDs[index] !== id) data[subtree_old_IDs[index]].wbs = wbs_new + data[subtree_old_IDs[index]].wbs.slice(wbs_current_length)
-        }
-    }
+//         for (let index in subtree_old_IDs) {
+//             if (subtree_old_IDs[index] !== id) data[subtree_old_IDs[index]].wbs = wbs_new + data[subtree_old_IDs[index]].wbs.slice(wbs_current_length)
+//         }
+//     }
 
     
-    // update relevant data
-    data[id].wbs = wbs_new
+//     // update relevant data
+//     data[id].wbs = wbs_new
 
 
-    if (parent_id !== null) data[parent_id]["hasChildren"] = true
+//     if (parent_id !== null) data[parent_id]["hasChildren"] = true
 
-    if (parent_id_old !== null) {
-        data[parent_id_old]["hasChildren"] = false
-        for (let index in data) {
-            if (data[index].parent === parent_id_old) {
-                data[parent_id_old]["hasChildren"] = true
-                break
-            }
-        }
-    }
-
-
-    // hide project if parent or its children are hidden
-    if (parent_id !== null) {
-        let index = id
-        while (true) {
-            index = data[index].parent
-            if (data[index].hidden || data[index].hiddenChildren) {
-                data[id].hidden = true
-                break
-            }
-            if (data[index].parent === null) break
-        }
-    }
-
-    // console.log('before sortby')
-    // console.log(data[id].wbs)
-    // console.log(data[id].parent)
-    // console.log(structuredClone(data))
-
-    // sort data array by WBS
-    sortByWBS(data)
+//     if (parent_id_old !== null) {
+//         data[parent_id_old]["hasChildren"] = false
+//         for (let index in data) {
+//             if (data[index].parent === parent_id_old) {
+//                 data[parent_id_old]["hasChildren"] = true
+//                 break
+//             }
+//         }
+//     }
 
 
-    // console.log('before reset index')
-    // console.log(data[id].wbs)
-    // console.log(data[id].parent)
-    // console.log(structuredClone(data))
+//     // hide project if parent or its children are hidden
+//     if (parent_id !== null) {
+//         let index = id
+//         while (true) {
+//             index = data[index].parent
+//             if (data[index].hidden || data[index].hiddenChildren) {
+//                 data[id].hidden = true
+//                 break
+//             }
+//             if (data[index].parent === null) break
+//         }
+//     }
 
-    // allocate new IDs (reset index)
-    resetIDs(data)
+//     // console.log('before sortby')
+//     // console.log(data[id].wbs)
+//     // console.log(data[id].parent)
+//     // console.log(structuredClone(data))
 
-    // console.log('after reset index')
-    // console.log(data[id].wbs)
-    // console.log(data[id].parent)
-    // console.log(structuredClone(data))
+//     // sort data array by WBS
+//     sortByWBS(data)
 
 
-    return true
-}
+//     // console.log('before reset index')
+//     // console.log(data[id].wbs)
+//     // console.log(data[id].parent)
+//     // console.log(structuredClone(data))
+
+//     // allocate new IDs (reset index)
+//     resetIDs(data)
+
+//     // console.log('after reset index')
+//     // console.log(data[id].wbs)
+//     // console.log(data[id].parent)
+//     // console.log(structuredClone(data))
+
+
+//     return true
+// }
 
 
 
@@ -309,6 +309,6 @@ function changeParent_v2(data, id, parent_id) { // assumption is the data is sor
 
 
 export function changeParent(data, id, parent_id) {
+    // return changeParent_v1(data, id, parent_id)
     return changeParent_v2(data, id, parent_id)
-    return changeParent_v1(data, id, parent_id)
 }
